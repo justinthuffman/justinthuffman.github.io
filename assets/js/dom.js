a2c2aa4d9b06b6495a6cebe0cc0038bdd67baabf
Justin Huffman <justin.t.huffman@gmail.com>
2018-10-28 22:18:59 -0400

    nav button works. project descriptions added

--- assets/js/dom.js
+++ assets/js/dom.js
@@ -1,1 +1,23 @@
-var navigationMenu = document.querySelectorAll('.navWithClose');
+
+
+// Nav button functions
+var fullNav = document.querySelector('.buttonFunction');
+var hiddenNav = document.querySelector('.buttonExpand');
+var closeButton = document.querySelector('.closebutton');
+var openButton = document.querySelector('.openbutton');
+
+
+
+function closeNav() {
+	fullNav.style.display = 'none';
+	hiddenNav.style.display = 'block';
+}
+
+function openNav() {
+	hiddenNav.style.display = 'none';
+	fullNav.style.display = 'block';
+
+}
+
+closeButton.onclick = closeNav;
+openButton.onclick = openNav;
