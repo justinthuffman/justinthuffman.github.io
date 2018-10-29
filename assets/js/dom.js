f757e1403850069f84236f1a8da2d937794cd39b
Justin Huffman <justin.t.huffman@gmail.com>
2018-10-29 00:36:27 -0400

    description button and bio modal added

--- assets/js/dom.js
+++ assets/js/dom.js
@@ -1,11 +1,14 @@
 
-
+//
 // Nav button functions
+//
+
+
+
 var fullNav = document.querySelector('.buttonFunction');
 var hiddenNav = document.querySelector('.buttonExpand');
 var closeButton = document.querySelector('.closebutton');
 var openButton = document.querySelector('.openbutton');
-
 
 
 function closeNav() {
@@ -21,3 +24,64 @@ function openNav() {
 
 closeButton.onclick = closeNav;
 openButton.onclick = openNav;
+
+
+
+
+//
+// Description Button Functions
+//
+
+
+
+var hiddenDescriptions = document.querySelector('.projectDescriptions');
+var showDescriptionsButton = document.querySelector('.showDescriptionButton');
+var hideDescriptionsButton = document.querySelector('.hideDescriptionButton');
+
+
+function showDescriptions() {
+	hiddenDescriptions.style.display = 'inline-block';
+	showDescriptionsButton.style.display = 'none';
+	hideDescriptionsButton.style.display = 'inline-block';
+}
+
+function hideDescriptions() {
+	hiddenDescriptions.style.display = 'none';
+	showDescriptionsButton.style.display = 'inline-block';
+	hideDescriptionsButton.style.display = 'none';
+
+}
+
+showDescriptionsButton.onclick = showDescriptions;
+hideDescriptionsButton.onclick = hideDescriptions;
+
+
+
+//
+// BIO Button Functions
+//
+
+
+var aboutMeLink = document.querySelector('.aboutMeLink');
+var modal = document.querySelector('.modal');
+var backdrop = document.querySelector('.backdrop');
+var modalButton = document.querySelector('.modalButton');
+
+
+function showModal() {
+	modal.style.display = 'block';
+	backdrop.style.display = 'block';
+}
+
+aboutMeLink.onclick = showModal;
+
+
+function hideModal() {
+	modal.style.display = 'none';
+	backdrop.style.display = 'none';
+}
+
+modalButton.onclick = hideModal;
+
+
+
